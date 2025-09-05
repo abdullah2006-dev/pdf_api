01131c6571428faf2ae3ee9ea7845926311490e1
Nayyar Abbas <nayyar.abbas@virtuosoft.pk>
2025-09-05 12:39:14 +0500

    build static url

--- blog/views.py
+++ blog/views.py
@@ -327,8 +327,8 @@ def create_comparatif_filename(society: str, trade_name: str, energy_type: str) 
 
 def build_static_url(request, path):
     print("Inside BuildStaticURL")
-    return request.build_absolute_uri(settings.STATIC_URL + path)
-
+    abs_path = os.path.join(settings.STATICFILES_DIRS[0], path)
+    return f"file://{abs_path}"
 
 def build_presentation_data(data, chart_base64, comparatif_dto, request):
     print("Inside BuildPresentationData")
