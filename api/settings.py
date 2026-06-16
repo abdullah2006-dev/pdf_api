953aecfe575f682c041eae1335ebc654771a532b
Nayyar Abbas <abbasnayyar63@gmail.com>
2026-06-16 12:29:54 +0500

    Update STATIC_URL to '/pdf-static/' for improved static file management

--- api/settings.py
+++ api/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/pdf-static/'
 
 # Add these lines for static files
 STATICFILES_DIRS = [
